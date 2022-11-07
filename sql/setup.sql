9cd59f7a6a8d0e5e0958b3f7564d2ee75adb34e6
Rio Edwards <rioredwards@gmail.com>
2022-11-07 15:41:54 -0800

    add static path for directory page

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,25 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE authors (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR NOT NULL,
+  dob DATE,
+  pob VARCHAR
+);
+
+CREATE TABLE books (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR NOT NULL,
+  released INT NOT NULL
+);
+
+INSERT INTO authors (name, dob, pob)
+VALUES 
+('cool author-name!', '1998-10-13', 'cool pob!');
+
+INSERT INTO books (title, released)
+VALUES
+('cool book-title!', 1994);
