cae0080f695ac9571d0aa9dd70e690c5ae2654db
Rio Edwards <rioredwards@gmail.com>
2022-11-08 12:55:11 -0800

    All Tests Passing! Setup route for /authors/id:

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -7,6 +7,18 @@ describe('authors routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  // eslint-disable-next-line
+  it(`/authors/id: should return a book and its author`, async () => {
+    const resp = await request(app).get('/authors/1');
+    expect(resp.status).toBe(200);
+    // expect(resp.body.length).toBe(4);
+    expect(resp.body).toEqual({
+      name: expect.any(String),
+      dob: expect.any(String),
+      pob: expect.any(String),
+      books: expect.any(Array),
+    });
+  });
 
   it('/authors should return a list of authors with id, title and release-date', async () => {
     const res = await request(app).get('/authors');
@@ -16,7 +28,6 @@ describe('authors routes', () => {
     expect(tolkein).toHaveProperty('dob', '1892-01-03');
     expect(tolkein).toHaveProperty('pob', 'Bloemfontein, Orange Free State');
   });
-
   afterAll(() => {
     pool.end();
   });
